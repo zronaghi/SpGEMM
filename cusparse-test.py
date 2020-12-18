5531c241f61639c20ec7a8afabc9452f8bf913d1
Zahra Ronaghi <zronaghi@gmail.com>
2020-12-18 17:01:02 -0600

    update cusparse

--- cusparse-test.py
+++ cusparse-test.py
@@ -1,22 +1,37 @@
 import cupy
-import scipy
+import scipy.sparse
+import time
 from cupy import cusparse
 from cupyx.scipy import sparse
-from scipy.io.mmio import mminfo, mmread, mmwrite
+from scipy.io import mmread
+from numba import cuda
 
-a = scipy.sparse.random(2, 3, density=0.5)
-b = scipy.sparse.random(3, 4, density=0.5)
+#a = scipy.sparse.random(2, 3, density=0.5)
+#b = scipy.sparse.random(3, 4, density=0.5)
 
-a2 = sparse.csr_matrix(a)
-b2 = sparse.csr_matrix(b)
+#a2 = sparse.csr_matrix(a)
+#b2 = sparse.csr_matrix(b)
 
 ## load mtx file
-# G = mmread(graph_file)
-# Gnx = nx.from_scipy_sparse_matrix(G, create_using=nx.DiGraph)
-# print(Gnx.number_of_edges())
+G = mmread('./ca-HepPh_adj.mtx')
+G = G+G.transpose()
+Gnx = sparse.csr_matrix(G)
+Gny = sparse.csr_matrix(G.transpose())
+#print(Gnx.number_of_edges())
+#print(type(Gnx))
+#print(type(Gny))
+#print(Gnx.shape)
+#print(Gny.shape)
 
-y = cusparse.csrgemm2(a2, b2)
+print("NNZ x:", Gnx.nnz)
+print("NNZ y:", Gny.nnz)
 
-print("\n a is: \n",a2)
-print("\n b is: \n",b2)
-print("\n y is: \n",y)
+t1 = time.time()
+y = cusparse.csrgemm2(Gnx, Gny)
+cuda.synchronize()
+t2 = time.time()
+
+print("total time is:", t2 - t1)
+print("NNZ:", y.nnz)
+
+#print("\n y is: \n",y)
